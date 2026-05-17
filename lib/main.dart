25201a07b8fbccfdf608445e91d9713d1ae59da8
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-05-17 17:16:20 +0545

    Learn about wedgits, sateless wedgits, stateful wedgits, AppBar, Scaffold

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewApp());
+}
+
+class NewApp extends StatelessWidget {
+  const NewApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+        ),
+      body: Text("Arun"),
+      )
+    );
+  }
+}
+
+
+
+
