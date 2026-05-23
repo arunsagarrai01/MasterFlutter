98da820181c21843631211a34aa9c7db14b4b4ab
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-05-23 18:45:58 +0545

    learn about row and column, container, child and children defference

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,33 @@ class NewApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: Text("Arun Sagar Rai"),
+          actions: [
+            Icon(Icons.home),
+            Icon(Icons.search),
+            Icon(Icons.heart_broken)
+          ],
+          leading: CircleAvatar(child: Text("A")),
         ),
-      body: Text("Arun"),
-      )
+      body: Column(children: [
+        Text("My name is Arun Sagar Rai"),
+        CircleAvatar(child: Text("A")),
+        Container(
+          width: 300,
+          height: 300,
+          color: Colors.pink,
+        )// Container
+      ],)
+      /*body: Center(child: Text(
+        "Arun Sagar Rai",
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 50,
+          fontWeight: FontWeight.bold,
+        ),
+      ),)*/
+      ),
     );
   }
 }
